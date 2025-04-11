8d3e273931ba7ebc9af9680baa9740ea5d3957c4
kamalesh <kamalikamalesh0401@gmail.com>
2025-04-11 12:19:37 +0530

    11-Apr-2025 homepage update

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -89,9 +89,33 @@
 
 import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
+import forest from '../assets/images/forest.jpg';
+import forest2 from '../assets/images/forest2.jpg';
+import forest3 from '../assets/images/forest3.jpg';
+import Slider from 'react-slick';
 import './HomePage.css';
 
+
 function HomePage() {
+
+    const sliderSettings = {
+        dots: true, // Show dots for navigation
+        infinite: true, // Loop the slider
+        speed: 500, // Transition speed
+        slidesToShow: 1, // Show 1 slide at a time
+        slidesToScroll: 1, // Scroll 1 slide at a time
+        autoplay: true, // Auto-play the slider
+        autoplaySpeed: 3000, // 3 seconds per slide
+        arrows: false, // Hide arrows (we'll use dots for navigation)
+        pauseOnHover: true, // Pause autoplay on hover
+        lazyLoad: 'ondemand', //Enable lazy loading
+    };
+    const sliderImages = [
+        forest2, forest3
+    ];
+    console.log(" sliderImages : ", sliderImages)
+    console.log(" process.env.PUBLIC_URL : ", process.env.PUBLIC_URL)
+
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleSidebar = () => {
@@ -119,8 +143,21 @@ function HomePage() {
                             <div className="intro-section">
                                 <h3 className="section-title">WELCOME</h3>
                                 <p className="description">
-                                    Discover the beauty of wildlife and national parks with Wildlife Explorer. Explore diverse ecosystems, learn about endangered species, and plan your next adventure...
+                                    Discover the beauty of wildlife and national parks with Wildlife Explorer, your ultimate guide to nature’s wonders. Immerse yourself in diverse ecosystems, from lush forests to sprawling wetlands, and learn about endangered species that call these habitats home. Whether you’re a nature enthusiast, a wildlife photographer, or an adventure seeker, our platform helps you explore breathtaking national parks, uncover fascinating facts about animals, and plan your next unforgettable journey into the wild. Join us in celebrating and preserving the natural world for generations to come!
                                 </p>
+                                <div className="mt-3 species-slider">
+                                    <Slider {...sliderSettings}>
+                                        {sliderImages.map((image, index) => (
+                                            <div key={index}>
+                                                <img
+                                                    src={image}
+                                                    alt={`Wildlife Scene ${index + 1}`}
+                                                    className="species-image"
+                                                />
+                                            </div>
+                                        ))}
+                                    </Slider>
+                                </div>
                             </div>
                         </div>
                         <div className="right-section">
